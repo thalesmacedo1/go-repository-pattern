076e3e5a8737fdffefb54e2f45c5c1de82b0689a
Thales Macêdo <thalesmacedo1@live.com>
2022-06-07 15:17:10 -0300

    adjusted main.go to run using sqlite

--- main.go
+++ main.go
@@ -1,18 +1,21 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 
-	"github.com/thalesmacedo1/go-repository-pattern/entity"
 	"github.com/thalesmacedo1/go-repository-pattern/repository"
 	"github.com/thalesmacedo1/go-repository-pattern/service"
 )
 
 func main() {
-	db := repository.CategoriesMemoryDb(Categories: []entity.Category{})
-	repositoryMemory := repository.NewCategoryRepositoryMemory(db)
+	// db := repository.CategoriesMemoryDb{Categories: []entity.Category{}}
+	// repositoryMemory := repository.NewCategoryRepositoryMemory(db)
 
-	service := service.NewCategoryService(repositoryMemory)
+	db, _ := sql.Open("sqlite3", "./sqlite.repository")
+	repositorySqlite := repository.NewCategorySqlite(db)
+
+	service := service.NewCategoryService(repositorySqlite)
 	cat, _ := service.Create("MY CATEGORY")
 
 	fmt.Println(cat)
